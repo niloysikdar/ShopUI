7c0cbee38aa3db2af55f781f98cbd7580a2cdfe3
Niloy Sikdar <niloysikdar30@gmail.com>
2021-02-24 16:15:16 +0530

    Added Appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
+import 'package:ShopUI/helper/constants.dart';
 import 'package:ShopUI/screens/homepage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: Theme.of(context).textTheme.apply(bodyColor: kTextColor),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
       home: HomePage(),
-    ),
-  );
+    );
+  }
 }
--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,3 +1,4 @@
+import 'package:ShopUI/helper/constants.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,6 +9,42 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: customAppbar(),
+    );
   }
+}
+
+Widget customAppbar() {
+  return AppBar(
+    backgroundColor: Colors.white,
+    elevation: 0,
+    leading: IconButton(
+      icon: Icon(
+        Icons.arrow_back_rounded,
+        size: 30.0,
+        color: kTextColor,
+      ),
+      onPressed: () {},
+    ),
+    actions: [
+      IconButton(
+        icon: Icon(
+          Icons.search,
+          size: 30.0,
+          color: kTextColor,
+        ),
+        onPressed: () {},
+      ),
+      IconButton(
+        icon: Icon(
+          Icons.shopping_cart_outlined,
+          size: 30.0,
+          color: kTextColor,
+        ),
+        onPressed: () {},
+      ),
+      SizedBox(width: 10.0),
+    ],
+  );
 }
